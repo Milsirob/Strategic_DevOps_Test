0ed9bedbf86e92338e281075a067a82aa94d5c84
LimBoris <borislim2@gmail.com>
2022-07-30 15:45:16 -0500

    modified greeting script

---
+++ greeting.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eo pipefail
+greeting() {
+ local _GITHUB_USER="${1}"
+ local _str="Hello, $_GITHUB_USER !"
+ echo $_str
+ return $?
+}
+greeting "$USER"
+RET=$?
+#if true || [ ${RET} -ne 0 ] ; then
+if [ ${RET} -ne 0 ] ; then    #Removed by default true check, it will always go to if block
+ echo 'ERR: Failed to give greeting' > /dev/stderr
+else
+ echo 'DEBUG: Greeted the Github User' > /dev/stderr
+fi
