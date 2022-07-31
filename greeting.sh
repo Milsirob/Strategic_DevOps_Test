5731fbb59b51b15248c18bc64ee83dd3fcc43261
LimBoris <borislim2@gmail.com>
2022-07-31 16:53:43 -0500

    modified greeting.sh script

--- greeting.sh
+++ greeting.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 greeting() {
- local _GITHUB_USER="${1}"
- local _str="Hello, $_GITHUB_USER !"
- echo $_str
- return $?
+  local _GITHUB_USER="${1}"
+  local _str="Hello, $_GITHUB_USER !"
+  echo $_str
+  return $?
 }
-greeting "$USER"
+greeting "Boris Lim"
 RET=$?
-#if true || [ ${RET} -ne 0 ] ; then
-if [ ${RET} -ne 0 ] ; then    #Removed by default true check, it will always go to if block
- echo 'ERR: Failed to give greeting' > /dev/stderr
+if [ ${RET} -ne 0 ] ; then
+  echo 'ERR: Failed to give greeting' > /dev/stderr
 else
- echo 'DEBUG: Greeted the Github User' > /dev/stderr
+  echo 'DEBUG: Greeted the Github User' > /dev/stdout
 fi
